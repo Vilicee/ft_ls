3033921e7d04eed29fc46b800bb4c1bd9a515d6b
wvaara <61557211+Vilicee@users.noreply.github.com>
2021-07-07 13:25:02 +0300

    fixed ft_address in ft_printf

--- libft/ft_printf/srcs/ft_address.c
+++ libft/ft_printf/srcs/ft_address.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_address.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wvaara <wvaara@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: wvaara <wvaara@hive.fi>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 14:50:42 by wvaara            #+#    #+#             */
-/*   Updated: 2021/04/19 16:12:36 by wvaara           ###   ########.fr       */
+/*   Updated: 2021/07/07 13:24:37 by wvaara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,6 @@ void	ft_address(t_data *data)
 	int		len;
 
 	ptr = va_arg(data->copy, void *);
-	if ((int)ptr == 0)
-		data->d_zero = 1;
 	len = ft_addr_len(ptr);
 	if (len == 10)
 	{
